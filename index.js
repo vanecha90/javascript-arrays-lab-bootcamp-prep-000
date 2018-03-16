f34d62d275d177fea8767a873813f2e3a4026053
vanessa.collado90@gmail.com <vanessa.collado90@gmail.com>
2018-03-16 20:30:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
- function destructivelyAppendKitten(name){
+ function destructivelyAppendKitten( Milo, Otis, Garfield){
     kittens = ['Milo', 'Otis', 'Garfield']
  }
  
